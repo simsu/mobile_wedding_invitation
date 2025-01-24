5777e6a51e34df0c8d8c7196bd3d453f6abe7448
Hyeoncheol Nam <hyeoncheol.nam@zeroum.co.jp>
2025-01-25 00:44:38 +0900

    오디오 자동재생 처리

--- src/layout/Main/AudioButton.tsx
+++ src/layout/Main/AudioButton.tsx
@@ -1,43 +1,45 @@
-import styled from '@emotion/styled';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Volume from '@/assets/icons/volume.svg';
 import VolumeOff from '@/assets/icons/volume-off.svg';
 
 function AudioButton() {
     const audioRef = useRef<HTMLAudioElement | null>(null);
-    const [isPlaying, setIsPlaying] = useState(false);
+    const [isPlaying, setIsPlaying] = useState(true);
 
     const handlePlayAudio = () => {
-        if (audioRef.current) {
-          if (isPlaying) {
-            audioRef.current.pause();
-          } else {
-            audioRef.current.play();
-          }
-          setIsPlaying(!isPlaying);
+      if (audioRef.current) {
+        if (audioRef.current.paused) {
+          audioRef.current.play();
+          setIsPlaying(true);
+        } else {
+          audioRef.current.pause();
+          setIsPlaying(false);
         }
+      }
     };
+
+    const playAudio = async () => {
+      if (audioRef.current) {
+        try {
+          await audioRef.current.play();
+        } catch {
+          setIsPlaying(false);
+        }
+      }
+    };
+
+    useEffect(() => {
+      playAudio();
+    }, []);
 
     return (
         <div style={{position: 'absolute', top: '0', right:'0'}}>
-            <Button onClick={handlePlayAudio} className=''>
-                <Img src={(audioRef.current == null) || isPlaying ? Volume : VolumeOff} />
-            </Button>
-            <audio ref={audioRef} src="/bgm.mp3" autoPlay loop></audio>
+          <button onClick={handlePlayAudio} className='cursor-pointer'>
+            <img src={isPlaying ? Volume : VolumeOff} alt="audio" />
+          </button>
+          <audio ref={audioRef} src="/bgm.mp3" loop></audio>
         </div>
     )
 }
 
 export default AudioButton;
-
-const Button = styled.button`
-  border: none;
-  outline: none;
-  background: transparent;
-  padding: 0;
-  font-family: inherit;
-  font-size: inherit;
-`;
-
-const Img = styled.img`
-`;
