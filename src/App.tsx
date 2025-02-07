bbf08fec181fa195843f2942623b467cc6cf8a02
simsu <dvt18@naver.com>
2025-02-07 22:36:12 +0900

    never use 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useState } from 'react';
 import { NavermapsProvider } from 'react-naver-maps';
 import { Heading1 } from '@/components/Text.tsx';
 import Wrapper from '@/components/Wrapper.tsx';
